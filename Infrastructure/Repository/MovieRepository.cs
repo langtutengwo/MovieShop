a1f095ed545224f395a06bfddd9542a473b6ad92
langtutengwo <langtutengwo@gmail.com>
2022-06-13 22:14:51 -0400

    Add project files.

---
+++ Infrastructure/Repository/MovieRepository.cs
@@ -0,0 +1,14 @@
+﻿using ApplicationCore.Contracts.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository
+{
+    public class MovieRepository: IMovieRepository
+
+    {
+    }
+}
